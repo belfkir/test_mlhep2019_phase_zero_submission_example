708a3bedd8fc207e99a721cf20fde83bd13f5c89
belfkir <belfkir.mohamed.95@gmail.com>
2019-06-28 10:33:51 +0200

    test5

--- solution.py
+++ solution.py
@@ -11,7 +11,7 @@ def main():
     df = pd.DataFrame(df, columns=['column 1', 'column 2'])
     df['result'] = df['column 1'] + df['column 2']
     np.savetxt(output_dir + '/data.predict', np.array(df['result']))
-    print "Hello word 3"
+    #print "Hello word 3"
     return 0
 
 if __name__ == "__main__":
